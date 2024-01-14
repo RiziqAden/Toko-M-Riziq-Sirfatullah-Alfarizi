0774eca58322f6331d55f231d12d05b41ab4c95e
M. Riziq Sirfatullah Alfarizi <115126841+RiziqAden@users.noreply.github.com>
2024-01-14 18:26:53 +0700

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2024 at 12:25 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_toko_riziq`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penerbit`
+--
+
+CREATE TABLE `tb_penerbit` (
+  `id_penerbit` varchar(4) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kota` varchar(20) NOT NULL,
+  `telepon` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_penerbit`
+--
+
+INSERT INTO `tb_penerbit` (`id_penerbit`, `nama`, `alamat`, `kota`, `telepon`) VALUES
+('SP01', 'Penerbit Informatika', 'Jl. Buah Batu No.121', 'Bandung', '0813-2220-1946'),
+('SP02', 'Andi Offset', 'Jl. Suryalaya IX No.3', 'Bandung', '0878-3903-0688'),
+('SP03', 'Danendra', 'Jl Moch. Toha 44', 'Bandung', '022-5201215');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_product`
+--
+
+CREATE TABLE `tb_product` (
+  `id_buku` varchar(20) NOT NULL,
+  `kategori` varchar(30) NOT NULL,
+  `nama_buku` varchar(30) NOT NULL,
+  `harga` int(15) NOT NULL,
+  `stok` int(3) NOT NULL,
+  `penerbit` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_product`
+--
+
+INSERT INTO `tb_product` (`id_buku`, `kategori`, `nama_buku`, `harga`, `stok`, `penerbit`) VALUES
+('B1001', 'Bisnis', 'Bisnis Online', 75000, 9, 'Penerbit Informatika'),
+('B1002', 'Bisnis', 'Etika Bisnis dan Tanggung Jawa', 68000, 20, 'Penerbit Informatika'),
+('K1001', 'Keilmuan', 'Analisis & Perancangan Sistem ', 50000, 60, 'Penerbit Informatika'),
+('K1002', 'Keilmuan', 'Artifical Intelligence', 45000, 60, 'Penerbit Informatika'),
+('K2003', 'Keilmuan', 'Autocad 3 Dimensi', 40000, 25, 'Penerbit Informatika'),
+('K3004', 'Keilmuan', 'Cloud Computing Technology', 85000, 15, 'Penerbit Informatika'),
+('N1001', 'Novel', 'Cahaya Di Penjuru Hati', 68000, 20, 'Andi Offset'),
+('N1002', 'Novel', 'Aku Ingin Cerita', 48000, 12, 'Danendra');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_penerbit`
+--
+ALTER TABLE `tb_penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indexes for table `tb_product`
+--
+ALTER TABLE `tb_product`
+  ADD PRIMARY KEY (`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
